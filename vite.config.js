a80e6712847220a2b1406f9d887be467284155f9
jose-willians <jose.willians@aluno.ufabc.edu.br>
2026-02-26 08:38:27 -0300

    ajuste

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/site-larissa-formaji/',
   plugins: [react()],
+  base: "/",  // ✅ para domínio próprio
 })
